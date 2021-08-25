cf89253cf5821139b20b8a673a4cd0da23aff05e
vagrant <rpg7ofdeath@gmail.com>
2021-08-25 20:01:00 +0000

    moved setupInput and handleUserInput to input.js and exported back to play.js

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const connect = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on('data', handleUserInput);
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect();

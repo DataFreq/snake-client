d6293475efdb49bdf5313129cc65f8ab7fe35cb2
vagrant <rpg7ofdeath@gmail.com>
2021-08-25 20:51:24 +0000

    added wasd + WASD movement

--- input.js
+++ input.js
@@ -1,16 +1,31 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.on('data', handleUserInput);
   stdin.resume();
+  stdin.on('data', handleUserInput);
   return stdin;
 };
 
 const handleUserInput = key => {
-  if (key === '\u0003') {
+  if (key.match('\u0003'))
     process.exit();
-  };
+
+  if (key.match('w') || key.match('W'))
+    connection.write('Move: up')
+
+  if (key.match('a') || key.match('A'))
+    connection.write('Move: left')
+
+  if (key.match('s') || key.match('S'))
+    connection.write('Move: down')
+    
+  if (key.match('d') || key.match('D'))
+    connection.write('Move: right')
+
 };
 
 module.exports = setupInput;

4d8a2f6d02f7967aa344328aa82badd0e7610ec5
vagrant <rpg7ofdeath@gmail.com>
2021-08-25 19:57:28 +0000

    added inital setup functions and quit cmd

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 const net = require('net');
 const connect = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on('data', handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();

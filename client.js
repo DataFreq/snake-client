2b1d970cff8c6845292e759e3bed8842b32e4d67
vagrant <rpg7ofdeath@gmail.com>
2021-08-25 19:38:44 +0000

    init commit of client.js and play.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    conn.write('Hello from Neo');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
